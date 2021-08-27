98ace94034d56d966cd9a25265c604d7ab3378e3
LT <liuting0109@126.com>
2021-08-27 11:44:14 +0800

    导航栏实现和图书管理页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 导入刚才编写的组件
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
+import AppIndex from '../components/home/AppIndex'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
 
 Vue.use(Router)
 
@@ -16,12 +18,29 @@ export default new Router({
       component: Login
     },
     {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
+      path: '/home',
+      name: 'Home',
+      // home页面并不需要被访问
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
     }
   ]
 })
